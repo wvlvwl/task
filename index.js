1a443e95eb0b89443065a0f4edd8ad777ce942b6
wvlvwl <37897884+wvlvwl@users.noreply.github.com>
2018-04-05 00:24:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ var http = require('http')
 
 http.createServer(function (request, response) {
 	response.writeHead(200, {"Content-Type": "text/plain"})
-	response.end("Hello, World!\n")
-	echo "<p>Текущие дата и время: " . date("r") . "</p>"
+	response.end("Hello, World!\n Current date and time:" . date("r") .)
 }).listen(process.env.PORT)
