1271c0dfb549abc3aabcf8effa8ce96aa153bb40
wvlvwl <37897884+wvlvwl@users.noreply.github.com>
2018-04-04 22:18:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http')
 
 http.createServer(function (request, response) {
 	response.writeHead(200, {"Content-Type": "text/plan"})
-	response.end("[COLOR=red]Hello, World![/COLOR]\n")
+	response.end('<p style="color:red">Hello, World!</p>\n')
 }).listen(process.env.PORT)
