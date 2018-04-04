dc0509e421291e794dbd0c3df4f0b13709cbb4e6
wvlvwl <37897884+wvlvwl@users.noreply.github.com>
2018-04-04 23:57:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var http = require('http')
-
-http.createServer(function (request, response) {
-	response.writeHead(200, {"Content-Type": "text/plan"})
-	response.end('Hello, World!\n')
-}).listen(process.env.PORT)
+<html>
+ <head>
+  <title>Тестируем PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Привет, мир!</p>'; ?>
+ </body>
+</html>
