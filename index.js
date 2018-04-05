66b3d205dece1b0f7f7feeef6aeda8a5ed711243
wvlvwl <37897884+wvlvwl@users.noreply.github.com>
2018-04-05 14:22:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http')
 
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"})
-  response.end("This is my first Heroku app\n")
+  response.end("Hello, World!\n")
 }).listen(process.env.PORT)
